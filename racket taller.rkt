f4d72f6c86c912d8de5a6da738fbf6fdfac3e8a4
kiin The Dovah <kiinthedovah@gmail.com>
2021-02-27 13:27:22 -0500

    16 untested

--- racket taller.rkt
+++ racket taller.rkt
@@ -233,7 +233,12 @@
                          (cond
                            [(null? lst) empty]
                            [(equal? (car lst) elem) 'car]
-                           [(list? (car lst)) (cons 'compose (cons (carCdr-resolve elem (car lst)) '(car)))]
+                           [(list? (car lst)) (if
+                                               (zero? contar-ocurrencias elem lst)
+                                               (cons 'compose (cons (carCdr-resolve elem (cdr lst)) '(cdr)))
+                                               (cons 'compose (cons (carCdr-resolve elem (car lst)) '(car)))
+                                               )
+                           ]
                            [else (cons 'compose (cons (carCdr-resolve elem (cdr lst)) '(cdr)))]
                            )
                          )
